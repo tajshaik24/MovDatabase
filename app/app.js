5e25db91fae2d6536f9c7a227e94398504aee193
Taj Shaik <tajshaik24@gmail.com>
2017-09-16 22:07:15 -0700

    wooo updates

--- app/app.js
+++ app/app.js
@@ -1,6 +1,11 @@
 'use strict'
 
 angular.module("movieDB", [
+    'ngRoute',
+    'movieDB.movie',
     'movieDB.catalog',
     'movieDB.paginate-filter'
-])
+]).config(['$routeProvider', function($routeProvider){
+    $routeProvider.when("/", {templateUrl: "catalog/catalog.html", controller: "catalogCtrl"}).when("/movie/:id", {templateUrl: "movie/movie.html", controller: "movieCtrl"}).otherwise({redirectTo: "/"})
+    
+}]);
